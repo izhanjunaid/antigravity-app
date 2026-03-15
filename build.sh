182f861c483872a0199865cd9a67bd68a5f40759
izhanjunaid <izhanjunaid123@gmail.com>
2026-03-15 17:26:56 +0500

    Fix Vercel buildCommand length limit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.0-stable.tar.xz | tar xJ
+export PATH=$PATH:`pwd`/flutter/bin
+flutter build web --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
