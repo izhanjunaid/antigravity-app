97b9bb88f0959a4224dd71e7bb7873065265b240
izhanjunaid <izhanjunaid123@gmail.com>
2026-03-16 14:31:33 +0500

    Update Flutter version to 3.41.4 for Vercel build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.0-stable.tar.xz | tar xJ
+curl -fsSL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.41.4-stable.tar.xz | tar xJ
 export PATH=$PATH:`pwd`/flutter/bin
 git config --global --add safe.directory `pwd`/flutter
 flutter build web --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
